f8511d471c6e7c43eee070eb4ddd1ac2d42849fa
murilero <murilo.dircksen.campos@escola.pr.gov.br>
2025-04-09 19:43:10 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE cliente 
+( 
+); 
+
+CREATE TABLE festas 
+( 
+); 
+
+CREATE TABLE pagamento 
+( 
+); 
+
+CREATE TABLE pix 
+( 
+ codigo pix INT,  
+); 
+
+CREATE TABLE boleto 
+( 
+ cod.barras INT,  
+ data de vencimento INT,  
+); 
+
+CREATE TABLE cartaocredito 
+( 
+ nome no cartao INT,  
+ endereço INT,  
+ cpf INT,  
+); 
+
